cc16258c26373b749b20bb9e106b327e1ed21962
Juho Taipale <krooh@hotmail.com>
2023-03-10 06:39:44 +0200

    added same tests for custom multiline blinker1.rle source file

--- test/inputparser.test.mjs
+++ test/inputparser.test.mjs
@@ -47,7 +47,22 @@ describe("Test input parser with glider", () => {
 
 describe("Test input parser with multiline state blinker", () => {
   const data = readInputFile("./test/blinker1.rle");
-  it("read state with many rows", () => {
-    expect(readState(data)).to.equal("3o!3o!");
+  it("read input file", () => {
+    expect(data.length).to.not.equal(0);
+  });
+  it("read width", () => {
+    expect(readWidth(data)).to.equal(3);
+  });
+  it("read height", () => {
+    expect(readHeight(data)).to.equal(2);
+  });
+  it("read state", () => {
+    expect(readState(data)).to.equal("3o$3o!");
+  });
+  it("test changing input state string to matrix", () => {
+    const w = readWidth(data);
+    const h = readHeight(data);
+    const state = readState(data);
+    expect(stateToMatrix(state, w, h)).to.deep.equal([[true, true, true],[true, true, true]]);
   });
 });
