f709d5e4be01e6fd03eac579c3f0f11d23eb2561
Juho Taipale <krooh@hotmail.com>
2023-03-10 10:36:01 +0200

    step function working

--- src/evolution.mjs
+++ src/evolution.mjs
@@ -30,3 +30,33 @@ export function shrink(matrix){
 function rotateMatrix(matrix){
   return matrix.length === 0 ? [] : matrix[0].map((val, index) => matrix.map(row => row[index]).reverse());
 }
+
+export function step(matrix){
+  let ematrix = expand(matrix);
+  const ewidth = ematrix[0].length;
+  const eheight = ematrix.length;
+  let res = [];
+  for (let r = 0; r < eheight; r++){
+    res[r] = Array(ewidth).fill(false);
+  }
+  for (let y = 0; y < eheight; y++){
+    for (let x = 0; x < ewidth; x++){
+      let aliveCount = 0;
+      for (let r = y-1; r <= y+1; r++){
+        for (let c = x-1; c <= x+1; c++){
+          let isalive = (!(r === y && c === x) && r > -1 && r < eheight && c > -1 && c < ewidth && ematrix[r][c])
+          if (isalive) {
+            aliveCount++;
+          }
+        }
+      }
+      if (!ematrix[y][x] && aliveCount === 3){
+        res[y][x] = true;
+      } else if (ematrix[y][x] && (aliveCount === 3 || aliveCount === 2)){
+        res[y][x] = true;
+      }
+    }
+  }
+  return shrink(res);
+}
+        
