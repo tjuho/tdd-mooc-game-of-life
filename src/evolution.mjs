89959fca8a451fd4c1d6cefe703abbd327dbafd7
Juho Taipale <krooh@hotmail.com>
2023-03-10 09:32:32 +0200

    expand func ready

--- src/evolution.mjs
+++ src/evolution.mjs
@@ -1,3 +1,14 @@
-export function sum(a, b) {
-  return a + b;
+export function expand(matrix) {
+  const owidth = matrix[0].length;
+  const oheight = matrix.length;
+  let res = [];
+  for (let r = 0; r < oheight+2; r++){
+    res[r] = Array(owidth+2).fill(false);
+  }
+  for (let r = 0; r < oheight; r++){
+    for (let c = 0; c < owidth; c++){
+      res[r+1][c+1] = matrix[r][c];
+    }
+  }
+  return res;
 }
--- test/evolution.test.mjs
+++ test/evolution.test.mjs
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { expand } from "../src/evolution.mjs";
 
 describe("Game of life evolution tests", () => {
-  it("Expand the area", () => {
-    expect([[true, true, true]]).to.equal([[false, false, false, false, false],[false, true, true, true, false],[false, false, false, false, false]]);
+  it("Expand the state area in every direction", () => {
+    expect(expand([[true, true, true]])).to.deep.equal([[false, false, false, false, false],[false, true, true, true, false],[false, false, false, false, false]]);
   });
 });
