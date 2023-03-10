a1b263e789cb893a4335663802b08e38ebf979f9
Juho Taipale <krooh@hotmail.com>
2023-03-10 10:47:37 +0200

    evolution tests with glider

--- test/evolution.test.mjs
+++ test/evolution.test.mjs
@@ -12,3 +12,17 @@ describe("Game of life evolution tests with blinker", () => {
     expect(step([[true, true, true]])).to.deep.equal([[true], [true], [true]]);
   });
 });
+
+describe("Game of life evolution tests with glider", () => {
+  const glidermat = [[false,true,false],[false,false,true],[true,true,true]];
+  const expanded = [[false, false, false, false, false],[false, false, true, false, false],[false, false, false, true, false],[false, true, true, true, false],[false, false, false, false, false]];
+  it("Expand the state area in every direction", () => {
+    expect(expand(glidermat)).to.deep.equal(expanded);
+  });
+  it("Shrink away empty horizontal and vertical lines from the sides of the area", () => {
+    expect(shrink(expanded)).to.deep.equal(glidermat);
+  });
+  it("Move a step", () => {
+    expect(step(glidermat)).to.deep.equal([[true,false,true], [false,true,true], [false,true,false]]);
+  });
+});
