8559ac6219d24904ae288ea6788a01c978a92781
Juho Taipale <krooh@hotmail.com>
2023-03-10 04:27:14 +0200

    tests for reading width and height added

--- test/inputparser.test.mjs
+++ test/inputparser.test.mjs
@@ -1,9 +1,17 @@
 import { expect } from "chai";
-import { readInputFile } from "../src/inputparser.mjs";
+import { readInputFile, readWidth, readHeight } from "../src/inputparser.mjs";
 
 describe("Test input parser", () => {
   it("read input file", () => {
     let data = readInputFile("./test/blinker.rle");
     expect(data.length).to.not.equal(0);
+  });
+  it("read width", () => {
+    let data = readInputFile("./test/blinker.rle");
+    expect(readWidth(data)).to.equal(3);
+  });
+  it("read height", () => {
+    let data = readInputFile("./test/blinker.rle");
+    expect(readHeight(data)).to.equal(1);
   });
 });
