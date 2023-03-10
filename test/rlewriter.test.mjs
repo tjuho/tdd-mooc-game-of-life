60e41f910fc6ea71155418e34ef7c79508105ecb
Juho Taipale <krooh@hotmail.com>
2023-03-10 11:30:51 +0200

    glider test with rle writer

--- test/rlewriter.test.mjs
+++ test/rlewriter.test.mjs
@@ -7,3 +7,10 @@ describe("Test RLE writer with blinker", () => {
     expect(matrixToRLE(matrix)).to.equal("3o!");
   });
 });
+
+describe("Test RLE writer with glider", () => {
+  const matrix = [[false,true,false],[false,false,true],[true,true,true]];
+  it("matrix to rle", () => {
+    expect(matrixToRLE(matrix)).to.equal("bob$2bo$3o!");
+  });
+});
