615a147a3fa75b380654ce39c01b5dbfcc835a8d
Juho Taipale <krooh@hotmail.com>
2023-03-10 06:36:52 +0200

    added same tests for glider.rle source file

--- test/inputparser.test.mjs
+++ test/inputparser.test.mjs
@@ -22,6 +22,29 @@ describe("Test input parser with blinker", () => {
     expect(stateToMatrix(state, w, h)).to.deep.equal([[true, true, true]]);
   });
 });
+
+describe("Test input parser with glider", () => {
+  const data = readInputFile("./test/glider.rle");
+  it("read input file", () => {
+    expect(data.length).to.not.equal(0);
+  });
+  it("read width", () => {
+    expect(readWidth(data)).to.equal(3);
+  });
+  it("read height", () => {
+    expect(readHeight(data)).to.equal(3);
+  });
+  it("read state", () => {
+    expect(readState(data)).to.equal("bob$2bo$3o!");
+  });
+  it("test changing input state string to matrix", () => {
+    const w = readWidth(data);
+    const h = readHeight(data);
+    const state = readState(data);
+    expect(stateToMatrix(state, w, h)).to.deep.equal([[false, true, false],[false, false, true],[true, true, true]]);
+  });
+});
+
 describe("Test input parser with multiline state blinker", () => {
   const data = readInputFile("./test/blinker1.rle");
   it("read state with many rows", () => {
