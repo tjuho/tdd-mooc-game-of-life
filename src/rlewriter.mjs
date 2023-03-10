a43fa3fa177ee04cfb7449d868d68249fb7b1bc7
Juho Taipale <krooh@hotmail.com>
2023-03-10 11:29:01 +0200

    blinker matrix to rle test pass

--- src/rlewriter.mjs
+++ src/rlewriter.mjs
@@ -1,4 +1,33 @@
-
 export function matrixToRLE(matrix) {
-  return "";
+  let res = "";
+  const width = matrix[0].length;
+  const height = matrix.length;
+  for (let y = 0; y < height; y++){
+    let prevch = '';
+    let ch = '';
+    let repeat = 1;
+    for (let x = 0; x < width; x++){
+      if (matrix[y][x]){
+        ch = 'o';
+      } else { 
+        ch = 'b';
+      }
+      if (!(x === width-1)){
+        if (!(prevch === ch) && !(prevch === '')){
+          res += (repeat > 1) ? repeat.toString()+ch : ch;
+          repeat = 1
+        } else {
+          repeat += 1;
+        }
+      } else if (x === width-1 && y === height-1){
+        res += (repeat > 1) ? repeat.toString()+ch : ch;
+        res += "!";
+      } else {
+        res += (repeat > 1) ? repeat.toString()+ch : ch;
+        res += "$";
+      }
+      prevch = ch;
+    }
+  }   
+  return res;
 }
--- test/rlewriter.test.mjs
+++ test/rlewriter.test.mjs
@@ -2,5 +2,8 @@ import { expect } from "chai";
 import { matrixToRLE } from "../src/rlewriter.mjs";
 
 describe("Test RLE writer with blinker", () => {
-  
+  const matrix = [[true, true, true]];
+  it("matrix to rle", () => {
+    expect(matrixToRLE(matrix)).to.equal("3o!");
+  });
 });
