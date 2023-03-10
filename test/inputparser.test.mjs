ccff5bdaefc44566d3171827e964fe334f7f4137
Juho Taipale <krooh@hotmail.com>
2023-03-10 06:32:54 +0200

    reformated the tests

--- test/inputparser.test.mjs
+++ test/inputparser.test.mjs
@@ -1,32 +1,30 @@
 import { expect } from "chai";
 import { readInputFile, readWidth, readHeight, readState, stateToMatrix } from "../src/inputparser.mjs";
 
-describe("Test input parser", () => {
+describe("Test input parser with blinker", () => {
+  const data = readInputFile("./test/blinker.rle");
   it("read input file", () => {
-    let data = readInputFile("./test/blinker.rle");
     expect(data.length).to.not.equal(0);
   });
   it("read width", () => {
-    let data = readInputFile("./test/blinker.rle");
     expect(readWidth(data)).to.equal(3);
   });
   it("read height", () => {
-    let data = readInputFile("./test/blinker.rle");
     expect(readHeight(data)).to.equal(1);
   });
   it("read state", () => {
-    let data = readInputFile("./test/blinker.rle");
     expect(readState(data)).to.equal("3o!");
   });
-  it("read state with many rows", () => {
-    let data = readInputFile("./test/blinker1.rle");
-    expect(readState(data)).to.equal("3o!3o!");
-  });
   it("test changing input state string to matrix", () => {
-    let data = readInputFile("./test/blinker.rle");
     const w = readWidth(data);
     const h = readHeight(data);
     const state = readState(data);
-    expect(stateToMatrix(data, w, h)).to.equal([[true, true, true]]);
+    expect(stateToMatrix(state, w, h)).to.deep.equal([[true, true, true]]);
+  });
+});
+describe("Test input parser with multiline state blinker", () => {
+  const data = readInputFile("./test/blinker1.rle");
+  it("read state with many rows", () => {
+    expect(readState(data)).to.equal("3o!3o!");
   });
 });
