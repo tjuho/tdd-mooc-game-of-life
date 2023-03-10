f9304599645423e2b54712188fe22a8a6dc48418
Juho Taipale <krooh@hotmail.com>
2023-03-10 04:50:32 +0200

    read width and height functions ready

--- src/inputparser.mjs
+++ src/inputparser.mjs
@@ -1,13 +1,15 @@
 import fs from "fs"
 
 export function readInputFile(path) {
-  return fs.readFileSync(path);
+  return fs.readFileSync(path, "utf-8");
 }
 
 export function readWidth(data) {
-  return -1;
+  let res = data.match(/\sx\s=\s(\d+)/);
+  return res ? parseInt(res[1]) : null;
 }
 
 export function readHeight(data) {
-  return -1;
+  let res = data.match(/\sy\s=\s(\d+)/);
+  return res ? parseInt(res[1]) : null;
 }
