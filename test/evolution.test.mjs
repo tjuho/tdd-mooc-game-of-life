be8b5b1fa72b81d13a37c25a0f19b041b4858462
Juho Taipale <krooh@hotmail.com>
2023-03-10 09:36:27 +0200

    test for shrink function

--- test/evolution.test.mjs
+++ test/evolution.test.mjs
@@ -1,8 +1,11 @@
 import { expect } from "chai";
-import { expand } from "../src/evolution.mjs";
+import { expand, shrink } from "../src/evolution.mjs";
 
 describe("Game of life evolution tests", () => {
   it("Expand the state area in every direction", () => {
     expect(expand([[true, true, true]])).to.deep.equal([[false, false, false, false, false],[false, true, true, true, false],[false, false, false, false, false]]);
+  });
+  it("Shrink away empty horizontal and vertical lines from the area", () => {
+    expect(shrink([[false, false, false, false, false],[false, true, true, true, false],[false, false, false, false, false]]).to.deep.equal([[true, true, true]]));
   });
 });
