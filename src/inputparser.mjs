5f401a9c93b2db99af676f5c8e6aba0474bd7592
Juho Taipale <krooh@hotmail.com>
2023-03-10 04:23:32 +0200

    read width and height function stubs added

--- src/inputparser.mjs
+++ src/inputparser.mjs
@@ -3,3 +3,11 @@ import fs from "fs"
 export function readInputFile(path) {
   return fs.readFileSync(path);
 }
+
+export function readWidth(data) {
+  return -1;
+}
+
+export function readHeight(data) {
+  return -1;
+}
